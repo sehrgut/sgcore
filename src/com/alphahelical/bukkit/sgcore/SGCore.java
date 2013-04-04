981f02c08bca71decf2d0c4702f57572460a03d8
Keith Beckman <kbeckman@becknet.com>
2013-04-04 17:19:41 -0400

    SGCore skeleton

---
+++ src/com/alphahelical/bukkit/sgcore/SGCore.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.alphahelical.bukkit.sgcore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author kbeckman
+ *
+ */
+public class SGCore extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+	
+}
